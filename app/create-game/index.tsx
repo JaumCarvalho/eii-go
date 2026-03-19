d3460b549d2b170b004037d89407e41f2f0170d3
João Carvalho <cjoao7610@gmail.com>
2026-03-19 17:35:57 -0300

    feat: implement CreateGame component with avatar selection and nickname input

--- app/create-game/index.tsx
+++ app/create-game/index.tsx
@@ -1,3 +1,55 @@
+import { Play, RefreshCw } from "lucide-react";
+import { useState, useEffect } from "react";
+
 export function CreateGame() {
-  return <></>;
+  const [nickname, setNickname] = useState("");
+  const [seed, setSeed] = useState("Random");
+  return (
+    <>
+      <main className="main-container">
+        <header className="logo-container">
+          <h1 className="logo-text">
+            EII<span className="logo-highlight">GO</span>
+          </h1>
+        </header>
+
+        <section className="profile-card">
+          <h3 className="instruction-text">ESCOLHA SEU AVATAR E NICKNAME</h3>
+
+          <div className="avatar-selection-wrapper">
+            <div className="avatar-display">
+              <img
+                src={`https://api.dicebear.com/8.x/adventurer/svg?seed=${seed}&backgroundColor=transparent`}
+                alt="Avatar"
+                className="avatar-img"
+              />
+            </div>
+            <button
+              className="btn-refresh"
+              aria-label="Mudar avatar"
+              onClick={() => {}}
+            >
+              <RefreshCw />
+            </button>
+          </div>
+
+          <div className="input-wrapper">
+            <input
+              type="text"
+              placeholder="NickName123"
+              className="input-nickname"
+              maxLength={15}
+              value={nickname}
+              onChange={(e) => setNickname(e.target.value)}
+            />
+          </div>
+
+          <button className="btn-create-room" onClick={() => {}}>
+            <Play />
+            CRIAR SALA
+          </button>
+        </section>
+      </main>
+    </>
+  );
 }
