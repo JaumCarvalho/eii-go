5d4340222244aec51be85e1bdbb96be35cd4ab3b
João Carvalho <cjoao7610@gmail.com>
2026-03-20 14:37:39 -0300

    feat: add routes for board and resultados components in routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/create-game.tsx"),
-
   route("lobby/:roomId", "routes/lobby.$roomId.tsx"),
+  route("board/:roomId", "routes/board.$roomId.tsx"),
+  route("resultados/:roomId", "routes/results.$roomId.tsx"),
 ] satisfies RouteConfig;
